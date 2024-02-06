96bec6cc380fe519ea2dd84f3e19a9d2f0719625
Gomson-E <gomsonixx@gmail.com>
2024-02-07 06:34:48 +0900

    FEAT : 파이썬 기초 문제풀이 - 41-1_문자열 함수

---
+++ PYTHON/PYTHON_BASIC_PRACTICE/41-1_stringFunction.py
@@ -0,0 +1,7 @@
+message = input('메시지 입력 : ')
+print('메시지 문자열 길이 : ', len(message))
+
+paragraph = '파이썬(영어: Python)은 1991년 프로그래머인 귀도 반 로섬이 발표한 고급 프로그래밍 언어로, 플랫폼에 독립적이며 인터프리터식, 객체지향적, 동적 타이핑(dynamically typed) 대화형 언어이다. 파이썬이라는 이름은 귀도가 좋아하는 코미디 <Monty Python\'s Flying Circus>에서 따온 것이다.'
+
+print('\'객체지향\' 문자열 위치: ', paragraph.find('객체지향'))
+
