b56ead6bb99394ce81a8e04349f81dd7542bb731
Gomson-E <gomsonixx@gmail.com>
2024-02-07 06:40:57 +0900

    FEAT : 파이썬 기초 문제풀이 - 44-1_키오스크 언어 선택

---
+++ PYTHON/PYTHON_BASIC_PRACTICE/44-1_kioskLanguageChoice.py
@@ -0,0 +1,5 @@
+choice = input('언어 선택(Choose your language): 1.한국어\t2.English')
+if choice == '1' :
+    print('1.샌드위치\t2.햄버거\t3.쥬스\t4.커피\t5.아이스크림')
+else:
+    print('1.Sandwitch\t2.Hamburger\t3.Juice\t4.Coffee\t5.Ice cream')
