4d8313dd3e2b76a847b93028ddf158bb699116ab
Gomson-E <gomsonixx@gmail.com>
2024-02-08 00:34:09 +0900

    REFACTOR : 파이썬 기초 문제풀이 - 42-1_넓이 계산 2

--- PYTHON/PYTHON_BASIC_PRACTICE/42-1_areaCirculation2.py
+++ PYTHON/PYTHON_BASIC_PRACTICE/42-1_areaCirculation2.py
@@ -1,8 +1,8 @@
 radius = int(input('반지름(cm) 입력 : '))
 
-print('원의 넓이\t: %.0f cm' % (radius ** 2 * 3.14))
-print('원 둘레길이\t: %.0f cm' % (radius * 2 * 3.14))
-print('원의 넓이\t: %.1f cm' % (radius ** 2 * 3.14))
+print('원의 넓이\t\t: %d cm' % (radius ** 2 * 3.14))
+print('원 둘레길이\t: %d cm' % (radius * 2 * 3.14))
+print('원의 넓이\t\t: %.1f cm' % (radius ** 2 * 3.14))
 print('원 둘레길이\t: %.1f cm' % (radius * 2 * 3.14))
-print('원의 넓이\t: %.3f cm' % (radius ** 2 * 3.14))
+print('원의 넓이\t\t: %.3f cm' % (radius ** 2 * 3.14))
 print('원 둘레길이\t: %.3f cm' % (radius * 2 * 3.14))
