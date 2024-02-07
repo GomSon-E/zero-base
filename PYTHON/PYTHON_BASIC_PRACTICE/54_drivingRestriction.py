f7c8941223b37f6d8a033cb3c8fb588bf4361343
Gomson-E <gomsonixx@gmail.com>
2024-02-07 21:05:26 +0900

    FEAT : 파이썬 기초 문제풀이 - 54_차량 운행 제한

---
+++ PYTHON/PYTHON_BASIC_PRACTICE/54_drivingRestriction.py
@@ -0,0 +1,27 @@
+import datetime
+
+fineDust = int(input('미세먼지 수치 입력 : '))
+vehicleType = int(input('차량 종류 선택. 1.승용차\t2.영업용차'))
+vehicleNumber = int(input('차량 번호 입력 : '))
+endNumber = vehicleNumber % 10
+
+now = datetime.datetime.now()
+
+print('-' * 25)
+print(now.weekday())
+print('-' * 25)
+
+if fineDust <= 150 or vehicleType == 2 :
+    if endNumber == now.weekday() + 1 or endNumber == now.weekday() + 6 :
+        print('금일 운행이 가능합니다!')
+    else :
+        print('차량 5부제 적용')
+        print('차량 5부제로 금일 운행제한 대상 차량입니다.')
+else :
+    if (now.day % 2 == 0 and endNumber % 2 == 0) or (now.day % 2 != 0 and endNumber % 2 != 0) :
+        print('금일 운행이 가능합니다!')
+    else :
+        print('차량 2부제 적용')
+        print('차량 2부제로 금일 운행제한 대상 차량입니다.')
+
+print('-' * 25)
