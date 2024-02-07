a53fc3a0296cd4773b077d2c93d997327ddb8884
Gomson-E <gomsonixx@gmail.com>
2024-02-08 01:24:57 +0900

    REFACTOR : 파이썬 기초 문제풀이 - 54_차량 운행 제한

--- PYTHON/PYTHON_BASIC_PRACTICE/54_drivingRestriction.py
+++ PYTHON/PYTHON_BASIC_PRACTICE/54_drivingRestriction.py
@@ -18,7 +18,7 @@ if fineDust <= 150 or vehicleType == 2 :
         print('차량 5부제 적용')
         print('차량 5부제로 금일 운행제한 대상 차량입니다.')
 else :
-    if (now.day % 2 == 0 and endNumber % 2 == 0) or (now.day % 2 != 0 and endNumber % 2 != 0) :
+    if (now.day % 2) == (endNumber % 2) :
         print('금일 운행이 가능합니다!')
     else :
         print('차량 2부제 적용')
