aa2875dac20fdf40079655cb26ab0eef0851cd96
GomSon-E <gomsonixx@gmail.com>
2024-02-16 22:17:24 +0900

    FEAT : 파이썬 중급 문제풀이 - 41_이동거리 계산기

---
+++ python/intermediate-practice/41_distanceCalculator.py
@@ -0,0 +1,15 @@
+def getDistance(speed, hour, minute):
+    return speed * (hour + minute / 60)
+
+
+print('-' * 60)
+
+s = float(input('속도(km/h) 입력 : '))
+h = float(input('시간(h) 입력 : '))
+m = float(input('시간(m) 입력 : '))
+
+d = getDistance(s, h, m)
+
+print(f'{s}(km/h)속도로 {h}(h)시간 {m}(m)분 동안 이동한 거리: {d}(km)')
+
+print('-' * 60)
