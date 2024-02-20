ad9a0ba2eb14f0706cb7250bffc3272a6d7479dc
GomSon-E <gomsonixx@gmail.com>
2024-02-20 15:36:10 +0900

    UPDATE : 파이썬 중급 문제풀이 - 66_가계부 - 잔액 텍스트 파일에 기록으로 변경

--- python/intermediate-practice/66_budgetBookEx.py
+++ python/intermediate-practice/66_budgetBookEx.py
@@ -1,6 +1,12 @@
 import time
 
-total = 0
+def readTotal():
+    with open('files/total.txt', 'r', encoding='utf-8') as f:
+        return int(f.read())
+
+def writeTotal(t):
+    with open('files/total.txt', 'w', encoding='utf-8') as f:
+        f.write(str(t))
 
 def writeBook(w, c, m, t):
     with open('files/budgetBook.txt', 'a', encoding='utf-8') as f:
@@ -16,19 +22,29 @@ while True:
     if choice == 1:
         money = int(input('입금액 입력: '))
         content = input('입금 내역 입력: ')
-        total += money
+
+        remain = readTotal()
+        total = remain + money
+
+        writeTotal(total)
         writeBook('입금', content, money, total)
+
         print('입금 완료!!')
-        print(f'기존 잔액: {total - money}')
+        print(f'기존 잔액: {remain}')
         print(f'입금 후 잔액: {total}')
 
     elif choice == 2:
         money = int(input('출금액 입력: '))
         content = input('출금 내역 입력: ')
-        total -= money
+
+        remain = readTotal()
+        total = remain - money
+
+        writeTotal(total)
         writeBook('출금', content, money, total)
+
         print('출금 완료!!')
-        print(f'기존 잔액: {total + money}')
+        print(f'기존 잔액: {remain}')
         print(f'출금 후 잔액: {total}')
 
     elif choice == 3:
