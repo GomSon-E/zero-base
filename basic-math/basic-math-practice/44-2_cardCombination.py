3773dad24223a2c0ce8ff8804ba4c5818a7bea8c
GomSon-E <gomsonixx@gmail.com>
2024-02-22 17:18:36 +0900

    CHORE : 기초수학 문제풀이 - 44-2_카드 조합 - 오타 수정

--- basic-math/basic-math-practice/44-2_cardCombination.py
+++ basic-math/basic-math-practice/44-2_cardCombination.py
@@ -1,4 +1,4 @@
-def getPossibility(n, p):
+def getProbability(n, p):
     result = 1
 
     for i in range(p):
@@ -9,4 +9,4 @@ def getPossibility(n, p):
     return 1 / int(result) * 100
 
 
-print(f'카드 7장 중 3장을 선택했을 때, 3, 4,5가 동시에 선택될 수 있는 확률 : {round(getPossibility(7, 3), 2)}%')
+print(f'카드 7장 중 3장을 선택했을 때, 3, 4,5가 동시에 선택될 수 있는 확률 : {round(getProbability(7, 3), 2)}%')
